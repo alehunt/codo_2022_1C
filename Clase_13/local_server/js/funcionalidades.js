843a69173cb5d480fc3a80b2156b4379992966cd
Alejandro <alehunt@gmail.com>
2022-04-18 19:34:10 -0300

    Clase 13 subida

---
+++ Clase_13/local_server/js/funcionalidades.js
@@ -0,0 +1,35 @@
+console.log("Estas accediendo a código secreto");
+console.warn("Estas accediendo a código secreto");
+console.error("Estas accediendo a código secreto");
+
+// var miVariable = 2;
+/*
+
+*/
+
+const pi = 20;
+let miVariable = 2;
+console.log(miVariable);
+console.log(typeof miVariable);
+//let otraVariable = 0.2;
+//pi = 10;
+miVariable = "pepe";
+console.log(miVariable);
+console.log(typeof miVariable);
+
+miVariable = 20 + 5;
+console.log(miVariable);
+console.log(miVariable + 20);
+
+miVariable = "2";
+//let valorIngresado = prompt("Ingrese un valor");
+let valorIngresadoEntero = parseInt(prompt("Ingrese un valor entero"));
+let valorIngresadoFloat = parseFloat(prompt("Ingrese un valor decimal"));
+
+
+
+
+
+
+
+
